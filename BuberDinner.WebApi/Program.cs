f0c0af2b7381db02c967ac723d6c5bccb01985ee
imanechrj <cherrajimane@icloud.com>
2024-03-31 18:03:12 +0200

    First commit

---
+++ BuberDinner.WebApi/Program.cs
@@ -0,0 +1,24 @@
+﻿
+namespace BuberDinner.WebApi;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        {
+            builder.Services.AddControllers();
+
+        }
+
+        var app = builder.Build();
+        {
+            app.UseHttpsRedirection();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
+

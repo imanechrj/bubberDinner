71e1eeb15d81b49a06ea9a29a74c6c4a8847cf8b
imane cherraj <cherrajimane@icloud.com>
2024-04-04 16:10:03 +0200

    Recadrage avec gitgnore

--- BuberDinner.Infrastructure/Authentication/JwtSettings.cs
+++ BuberDinner.Infrastructure/Authentication/JwtSettings.cs
@@ -3,12 +3,12 @@ namespace BuberDinner.Infrastructure.Authentication;
 public class JwtSettings
 {
     public const string SectionName = "JwtSettings";
-    public string Secret { get; init; }
+    public string? Secret { get; init; }
 
-    public int ExpiryMinutes { get; init; }
+    public int? ExpiryMinutes { get; init; }
 
-    public string Issuer { get; init; }
+    public string? Issuer { get; init; }
 
-    public string Audience { get; init; }
+    public string? Audience { get; init; }
 
 }
